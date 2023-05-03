bd3ef03b5e77a2b132fbca2b0e2a4273f75d771c
ebe <luishigareda@cbtis52.edu.mx>
2023-05-02 21:00:34 -0600

    commit parcial

--- IniciosesionFYM/app.js
+++ IniciosesionFYM/app.js
@@ -1,35 +1,9 @@
-const correo = document.getElementById("name")
-const contraseña = document.getElementById("name")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
-
-
-form.addEventListener("submit", e=> {
-
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\ .\w{2,3})+$/
-    parrafo.innerHTML = ""
-    if(nombre.nodeValue.lenght <6){
-    warnings += 'El email no existe <br>'
-    entrar = true
-    }
- 
-    if (!regexEmail.test(email.value)){
-        warnings += 'El email no es valido <br>'
-        entrar = true
-
-    }
-    if(Pass.value.lenght <8){
-
-        warnings += 'La contraseña no es valida <br>'
-        entrar = true
-
-    }
-    if (entrar){
-        parrafo.innerHTML = warnings
+function validar(){
+    var usuario = document.getElementById("usuario").value;
+    var password = document.getElementById("password").value;
+    if(usuario == "admin" && password == "findyourmovie"){
+        window.location.href = "file:///C:/Users/luise/Documents/FYM-new-master/FYMYOSHIO/FYM.html";
     }else{
-        parrafo.innerHTML = "Enviado"
+        alert("Usuario o contraseña incorrectos");
     }
-  }  )
+}
